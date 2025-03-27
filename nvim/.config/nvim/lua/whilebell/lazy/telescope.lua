5b6812ab3852069d03e40dbc603c6c691024caf7
WhileBELL <bellbelieve.work@gmail.com>
2025-03-28 06:00:34 +0700

    Make a telescope visual dotfiles and hidden files.

--- nvim/.config/nvim/lua/whilebell/lazy/telescope.lua
+++ nvim/.config/nvim/lua/whilebell/lazy/telescope.lua
@@ -8,22 +8,29 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
-
+        require('telescope').setup({
+            defaults = {},
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    no_ignore = true
+                }
+            }
+        })
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find files' })
+        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find git files' })
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
-        end)
+        end, { desc = 'Search for word under cursor' })
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
-        end)
+        end, { desc = 'Search for WORD under cursor' })
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        end, { desc = 'Search with prompt' })
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = 'View help tags' })
     end
 }
